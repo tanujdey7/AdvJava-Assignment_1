730c106a64bf40f9f5892ee807735d55f72f850e
Tanuj Dey <tanujdey7@gmail.com>
2019-07-28 16:36:34 +0530

    Delete 10.java

--- 10.java
+++
@@ -1,8 +0,0 @@
-/*Write a Java program to read one file byte by byte and write all those bytes to new file
-using file input/output stream.*/
-import java.io.*;
-class demo {
-    public static void main(String[] args) {
-        
-    }
-}
